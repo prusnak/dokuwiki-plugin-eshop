39f72445765d10d273264797dd125753f34003a4
Pavol Rusnak <stick@gk2.sk>
2013-06-18 03:08:54 +0200

    fix typo

--- syntax.php
+++ syntax.php
@@ -10,7 +10,7 @@ if(!defined('DOKU_INC')) define('DOKU_INC',realpath(dirname(__FILE__).'/../../')
 if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 require_once(DOKU_PLUGIN.'syntax.php');
 
-class syntax_plugin_nextday extends DokuWiki_Syntax_Plugin {
+class syntax_plugin_eshop extends DokuWiki_Syntax_Plugin {
 
     function getType() { return 'substition'; }
 
