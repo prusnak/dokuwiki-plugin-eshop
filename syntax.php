2c72a31b2dc367f4efb278cd808db3727b3c364e
Pavol Rusnak <stick@gk2.sk>
2013-06-20 15:46:08 +0200

    move rendering to render method

--- syntax.php
+++ syntax.php
@@ -35,30 +35,30 @@ class syntax_plugin_eshop extends DokuWiki_Syntax_Plugin {
         $btcusd = $this->getConf('btcusd');
         $eurusd = $this->getConf('eurusd');
 
-        $id = $data['id'];
-        $name = $data['name'];
-        $price_btc = $data['btc'];
-        $price_usd = $data['usd'];
-
-        if (!$id) return 'ESHOP ERROR: id missing';
-
-        if (!$name) return 'ESHOP ERROR: name missing';
-
-        if (!$price_btc && !$price_usd) return 'ESHOP ERROR: Both btc and usd prices missing';
-
-        if (!$price_btc) {
-            $price_btc = $price_usd / $btcusd;
+        if ($data['usd'] && $btcusd && !$data['btc']) {
+            $data['btc'] = $data['usd'] / $btcusd;
+        } else
+        if ($data['btc'] && $btcusd && !$data['usd']) {
+            $data['usd'] = $data['btc'] * $btcusd;
         }
-        if (!$price_usd) {
-            $price_usd = $price_btc * $btcusd;
+        if ($data['usd'] && $eurusd) {
+            $data['eur'] = $data['usd'] / $eurusd;
         }
-        $price_eur = $price_usd / $eurusd;
+        if (!$data['btc']) $data['btc'] = 0.0;
+        if (!$data['eur']) $data['eur'] = 0.0;
+        if (!$data['usd']) $data['usd'] = 0.0;
+        return $data;
+    }
 
+    function render($mode, &$renderer, $data) {
+        if ($data === false || $mode != 'xhtml') return false;
+
+        global $INFO;
         $out  = '<form method="post" action="">';
         $out .= '<table class="eshop_plugin">';
-        $out .= sprintf('<tr><th>Price in USD:</th><td class="price" id="eshop_price_usd" data-unitprice="%f">%0.2f</td></tr>', $price_usd, $price_usd);
-        $out .= sprintf('<tr><th>Price in EUR:</th><td class="price" id="eshop_price_eur" data-unitprice="%f">%0.2f</td></tr>', $price_eur, $price_eur);
-        $out .= sprintf('<tr><th>Price in BTC:</th><td class="price" id="eshop_price_btc" data-unitprice="%f">%0.3f</td></tr>', $price_btc, $price_btc);
+        $out .= sprintf('<tr><th>Price in USD:</th><td class="price" id="eshop_price_usd" data-unitprice="%f">%0.2f</td></tr>', $data['usd'], $data['usd']);
+        $out .= sprintf('<tr><th>Price in EUR:</th><td class="price" id="eshop_price_eur" data-unitprice="%f">%0.2f</td></tr>', $data['eur'], $data['eur']);
+        $out .= sprintf('<tr><th>Price in BTC:</th><td class="price" id="eshop_price_btc" data-unitprice="%f">%0.3f</td></tr>', $data['btc'], $data['btc']);
         $out .= '<tr><th>Quantity:</th><td class="count"><select name="count" id="eshop_count">';
         for ($i = 1; $i <= 10; $i++) {
             $out .= sprintf('<option value="%d">%d</option>', $i, $i);
@@ -66,18 +66,13 @@ class syntax_plugin_eshop extends DokuWiki_Syntax_Plugin {
         $out .= '</select></td></tr>';
         $out .= '<tr><td colspan="2" class="button"><input class="submit" type="submit" value="Buy"/></td></tr>';
         $out .= '</table>';
-        $out .= sprintf('<input type="hidden" name="id" value="%s" />', $id);
-        $out .= sprintf('<input type="hidden" name="name" value="%s" />', $name);
-        $out .= sprintf('<input type="hidden" name="btcunit" value="%f" />', $price_btc);
-        $out .= sprintf('<input type="hidden" name="btctotal" id="eshop_total" value="%f" />', $price_btc);
+        $out .= sprintf('<input type="hidden" name="id" value="%s" />', end(explode(':', $INFO['id'])));
+        $out .= sprintf('<input type="hidden" name="name" value="%s" />', $INFO['meta']['title']);
+        $out .= sprintf('<input type="hidden" name="btcunit" value="%f" />', $data['btc']);
+        $out .= sprintf('<input type="hidden" name="btctotal" id="eshop_total" value="%f" />', $data['btc']);
         $out .= '</form>';
 
-        return $out;
-    }
-
-    function render($mode, &$renderer, $data) {
-        if ($data === false || $mode != 'xhtml') return false;
-        $renderer->doc .= $data;
+        $renderer->doc .= $out;
         return true;
     }
 
