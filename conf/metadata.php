80f6445152917172de4a72e62ef8e9dba33859f4
Pavol Rusnak <stick@gk2.sk>
2013-06-20 02:10:54 +0200

    add mpk metadata

--- conf/metadata.php
+++ conf/metadata.php
@@ -1,4 +1,6 @@
 <?php
 $meta['btcusd'] = array('string');
 $meta['eurusd'] = array('string');
+$meta['masterpubkey'] = array('string');
+$meta['masterpubkeyindex'] = array('numeric');
 ?>
--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,4 +1,6 @@
 <?php
 $lang['btcusd'] = 'BTC/USD exchange rate';
 $lang['eurusd'] = 'EUR/USD exchange rate';
+$lang['masterpubkey'] = 'Master Public Key (64-bit hexstring)';
+$lang['masterpubkeyindex'] = 'Master Public Key Index (don\'t change unless you know what you are doing!)';
 ?>
